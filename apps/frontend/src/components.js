47d344da8235069470c64669a235db5e329cfd39
GouravSharmaa09 <gouravsharmaa09@gmail.com>
2025-07-19 12:50:16 -0400

    code changes

--- apps/frontend/src/components.js
+++
@@ -1,8 +0,0 @@
-// Export components from components.jsx
-export { ExclusiveOnboarding, ExclusiveRegistrationSuccess } from './components.jsx';
-
-// Export AdminPanel component
-export { default as AdminPanel } from './components/AdminPanel.jsx';
-
-// Export SimpleAuth component
-export { default as SimpleAuth } from './components/SimpleAuth.jsx'; 
